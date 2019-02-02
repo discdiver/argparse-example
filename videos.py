39f09029ab73c566ea4618cbd9c0803d0de8b6c4
discdiver <jeffmshale@gmail.com>
2019-02-01 23:17:54 -0500

    fix videos so no default testing

--- videos.py
+++ videos.py
@@ -1,8 +1,8 @@
 # videos.py
 import argparse
 parser = argparse.ArgumentParser(description='Videos to images')
-parser.add_argument('indir', type=int, help='Input dir for videos', default=2)
-parser.add_argument('outdir', type=int, help='Output dir for image', default=3)
+parser.add_argument('indir', type=str, help='Input dir for videos')
+parser.add_argument('outdir', type=str, help='Output dir for image')
 args = parser.parse_args()
 
 print(args.indir)
